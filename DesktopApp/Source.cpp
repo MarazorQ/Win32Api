eab931a88edf8ac820ec161d0e908f964f0356b6
Artem Shkuratov <shuratov2001@mail.ru>
2020-09-09 17:16:55 +0300

    v.1 Window

---
+++ DesktopApp/Source.cpp
@@ -0,0 +1,89 @@
+#include<windows.h>
+
+//Создаём прототип функции окна, которая будет определена ниже
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//объявляем строку-имя программы
+char szProgName[] = "Имя программы";
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND hWnd;
+	MSG lpMsg;
+	WNDCLASS w; //создаём экземпляр структуры WNDCLASS
+
+	//И начинаем её заполнять
+	w.lpszClassName = szProgName; //имя программы - объявлено выше
+	w.hInstance = hInstance; //идентификатор текущего приложения
+	w.lpfnWndProc = WndProc; //указатель на функцию окна
+	w.hCursor = LoadCursor(NULL, IDC_ARROW); //загружаем курсор
+	w.hIcon = 0; //иконки у нас не будет пока
+	w.lpszMenuName = 0; //и меню пока не будет
+	w.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //цвет фона окна
+	w.style = CS_HREDRAW | CS_VREDRAW; //стиль - перерисовываемое по х и по у
+	w.cbClsExtra = 0;
+	w.cbWndExtra = 0;
+
+	//Если не удалось зарегистрировать класс окна - выходим
+	if (!RegisterClass(&w))
+		return 0;
+
+	//Создадим окно в памяти, заполнив аргументы CreateWindow
+	hWnd = CreateWindow(szProgName, //Имя программы
+		"Моя первая программа!", //Заголовок окна
+		WS_OVERLAPPEDWINDOW, //Стиль окна - перекрывающееся
+		100, //положение окна на экране по х
+		100, //положение по у
+		500, //ширина
+		400, //высота
+		(HWND)NULL, //идентификатор родительского окна
+		(HMENU)NULL, //идентификатор меню
+		(HINSTANCE)hInstance, //идентификатор экземпляра программы
+		(HINSTANCE)NULL); //отсутствие дополнительных параметров
+
+		//Выводим окно из памяти на экран
+	ShowWindow(hWnd, nCmdShow);
+	//Обновим содержимое окна
+	UpdateWindow(hWnd);
+
+	//Цикл обработки сообщений
+
+	while (GetMessage(&lpMsg, hWnd, 0, 0)) { //Получаем сообщение из очереди
+		TranslateMessage(&lpMsg); //Преобразует сообщения клавиш в символы
+		DispatchMessage(&lpMsg); //Передаёт сообщение соответствующей функции окна
+	}
+	return(lpMsg.wParam);
+}
+
+//Функция окна
+LRESULT CALLBACK WndProc(HWND hWnd, UINT messg,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc; //создаём контекст устройства
+	PAINTSTRUCT ps; //создаём экземпляр структуры графического вывода
+
+	//Цикл обработки сообщений
+	switch (messg)
+	{
+		//сообщение рисования
+	case WM_PAINT:
+		//начинаем рисовать
+		hdc = BeginPaint(hWnd, &ps);
+		//здесь вы обычно вставляете свой текст:
+		TextOut(hdc, 150, 150,  "Здравствуй, WIN 32 API!!!!", 26);
+		//закругляемся
+		//обновляем окно
+		ValidateRect(hWnd, NULL);
+		//заканчиваем рисовать
+		EndPaint(hWnd, &ps);
+		break;
+
+		//сообщение выхода - разрушение окна
+	case WM_DESTROY:
+		PostQuitMessage(0); //Посылаем сообщение выхода с кодом 0 - нормальное завершение
+		break;
+
+	default:
+		return(DefWindowProc(hWnd, messg, wParam, lParam)); //освобождаем очередь приложения от нераспознаных
+	}
+	return 0;
+}
